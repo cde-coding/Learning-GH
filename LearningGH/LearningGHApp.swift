9a4cd457147356d367ef3bd6ce4ee0bc68e6944a
Christian Espinoza <improvementchimp@gmail.com>
2023-02-06 12:33:53 -0800

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Diego Espinoza on 2/6/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
