685968021254c65b0ce6bcd849f8e14efca458f9
cde-coding <124629232+cde-coding@users.noreply.github.com>
2023-02-06 12:58:42 -0800

    Update ContentView to What's Up!

--- LearningGH/ContentView.swift
+++ LearningGH/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, Fuckers!")
+            Text("What's up, CodeCrew!")
         }
         .padding()
     }
